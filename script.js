665b9860129c5d54efca639330e4ac6497c86ec4
amruta <pa17april@gmail.com>
2022-03-06 00:03:33 +0530

    docs: done

--- script.js
+++ script.js
@@ -6,19 +6,55 @@ function Book(name, author, type){
 }
 
 //display constructor
-function display(){
+function Display(){
+    
 
 }
 
 //add methods to display prototype
-display.prototype.clear=function(){
+Display.prototype.add=function(book){
+    tablebody=document.getElementById('tablebody');
+    let uistring=`<tr>
+  
+    <td> ${book.name}</td>
+    <td>${book.author}</td>
+    <td>${book.type}</td>
+  </tr>`
+
+  tablebody.innerHTML += uistring;
+  
+}
+
+
+
+
+Display.prototype.clear=function(){
     let libraryform= document.getElementById('libraryform');
     libraryform.reset();
 
 
 }
 
+Display.prototype.validate=function(book){
+    if(book.name.length===0 || book.author.length===0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
+Display.prototype.show=function(type,displaymeassage){
+    let meassage= document.getElementById('message')
+    meassage.innerHTML +=`<div class="alert ${type} alert-warning alert-dismissible fade show" role="alert">
+    <strong>Holy guacamole!</strong> ${displaymeassage}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`
+  setTimeout(function(){
+      meassage.innerHTML="";
+
+  },2000)
+}
 
 
 
@@ -50,7 +86,18 @@ function libraryformsubmit(e){
 
     let book= new Book(name,author,type);
 console.log(book);
+let display= new Display();
+if(display.validate(book)){
+display.add(book);
 display.clear();
+display.show('sucess','your book has been successfully added')
+}
+else{
+    //show error to the user
+    display.show('danger','sorry you cannnot add book')
+}
+
+
     
     console.log("you have submitted")
 }
